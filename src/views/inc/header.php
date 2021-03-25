ebe0238956b52dff7664d1cb96d361d5088d2df1
AndrisDirzininks <67545195+AndrisDirzininks@users.noreply.github.com>
2021-03-25 13:50:35 +0200

    Initial commit

---
+++ src/views/inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <script src="https://kit.fontawesome.com/aad56efa09.js" crossorigin="anonymous"></script>
+    <title>Magebit</title>
+</head>
+<body>
+    <div class="page-wrapper">
---
+++ src/views/pages/main-content.php
@@ -0,0 +1,42 @@
+        <div class="header">
+            <img class="grid-item" src="media\Union.png">
+            <img class="grid-item" src="media\pineapple.png">
+            <a class="grid-item" href="#">About</a>
+            <a class="grid-item" href="#">How it works</a>
+            <a class="grid-item" href="#">Contact</a>
+        </div>
+        <div class="row">
+            <div class="col first">
+                <div class="main-content">
+                    <div class="box">
+                        <h1>Subscribe to newsletter</h1>
+                        <h3>Subscribe to our newsletter and get 10% discount on pineapple glasses.</h3>
+                        <form action="#">
+                            <div class="mail-field">
+                                <input class="mail" type="email" id="" name="" value="" placeholder="Type your email address here…"><br>
+                                <input class="button" type="image" src="media\ic_arrow.svg" alt="Submit Form" />
+                                <input class="button-blue" type="image" src="media\ic_arrow_blue.svg" alt="Submit Form" />
+                                <p>email |</p>
+                            </div>
+                            <label class="check-box">
+                                <input type="checkbox" id="check" name="check" value="">
+                                <span class="checkmark"></span>
+                                <label class="check-label" for="check">I agree to <a href="#">terms of service</a></label>
+                            </label>
+                        </form>
+                        <div class="icons">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col second background">
+                <div class="">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
